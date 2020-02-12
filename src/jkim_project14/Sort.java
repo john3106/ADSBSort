5e3578e315d7649fb29bdd350815e67b90775da9
159672 <159672@10.72.7.126>
2020-02-12 09:06:12 -0500

    removed extra jumpsearch

--- src/jkim_project14/Sort.java
+++ src/jkim_project14/Sort.java
@@ -58,31 +58,4 @@ public class Sort {
 		}
 	}
 
-	static int jumpSearch(String[] arr, String s) {
-		// Find the optimal block size
-		int step = (int) Math.round(Math.sqrt(arr.length));
-		int b = step;
-		int a = 0;
-		int n = arr.length;
-
-		while (arr[Math.min(b,n)-1].compareTo(s) < 0) {
-			a = b;
-			b += step;
-			if (a >= n) {
-				return -1;
-			}
-		}
-		while (arr[a].compareTo(s)<0) {
-			a++;
-			if (a == Math.min(b,n)) {
-				return -1;
-			}
-		}
-		if (arr[a].equals(s)) {
-			return a;
-		} else {
-			return -1;
-		}
-	}
-
 }
